25f5e3401d39fbf30e55cf5b21375598511dd1ed
ThomasCardin <tomcardin@outlook.com>
2024-01-29 13:25:32 -0500

    issue #63: support merged branch and added .vscode

--- scripts/remove-old-image.py
+++ scripts/remove-old-image.py
@@ -1,6 +1,6 @@
 """
 
-This script is based on this documentation: 
+This script is based on this documentation:
 https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28
 
 """
@@ -9,7 +9,9 @@ import os
 import requests
 from requests.auth import HTTPBasicAuth
 
-# Delete the old container (based on the previous tag)
+"""
+Delete the old container (based on the previous tag)
+"""
 def delete_old_image(version_id, org, headers, auth):
     url_delete_previous_version = f"https://api.github.com/orgs/{org}/packages/container/{container_name}/versions/{version_id}"
     response = requests.delete(url_delete_previous_version, headers=headers, auth=auth)
@@ -18,26 +20,28 @@ def delete_old_image(version_id, org, headers, auth):
     else:
         print('error deleting the previous container:', response.status_code, response.text)
         exit(1)
-    
+
 """
 Find the previous tag for a specific container.
 Delete the previous tag if it exists and it is not the current one. This wont delete the current tag or older PR tags.
 Check if tags is not empty and check if the len is == 1. If that is the case, it means that the only tag is the previous one.
 """
-def find_previous_container_tag(response, pr_tag):
+def find_previous_container_tag(response, unique_tag):
     version_id = None
     for version in response:
         print(f"Found tags {version['metadata']['container']['tags']})")
         tags = version['metadata']['container']['tags']
-        if pr_tag not in tags and len(tags) == 1 and tags:
+        if unique_tag not in tags and len(tags) == 1 and tags:
             version_id = version['id']
             print(f"Previous tag found {tags[0]} with version_id {version_id}")
             return tags[0], version_id
 
     print(f"Container name {container_name} not found or the only tag found was the current one. If that is the case, you can ignore this error.")
     exit(0)
-        
-# Get all GCR container information
+
+"""
+Get all GCR containers information
+"""
 def get_container_tags(org, container_name, auth, headers, container_path):
     get_versions = f"https://api.github.com/orgs/{org}/packages/container/{container_name}/versions"
     response = requests.get(get_versions, headers=headers, auth=auth)
@@ -46,18 +50,18 @@ def get_container_tags(org, container_name, auth, headers, container_path):
     except requests.exceptions.HTTPError as e:
         print(f"Error getting the previous tag for the container {container_path} : {e}")
         exit(1)
-    
+
     return response
 
 def print_console(message):
     print("====================================")
     print(message)
-    
+
 if __name__ == "__main__":
     registry = os.getenv("REGISTRY")
     github_token = os.getenv("GITHUB_TOKEN")
     container_name = os.getenv("CONTAINER_NAME")
-    pr_tag = os.getenv("PR_TAG")
+    unique_tag = os.getenv("UNIQUE_TAG")
     user = os.getenv("USER")
     current_commit = os.getenv("CURRENT_COMMIT")
 
@@ -66,7 +70,7 @@ if __name__ == "__main__":
     }
     auth = HTTPBasicAuth(user, github_token)
 
-    container_path = f"{registry}/{container_name}:{pr_tag}"
+    container_path = f"{registry}/{container_name}:{unique_tag}"
     org = registry.split("/")[1]
 
     print_console(f"Getting all tags for this container {container_path}...")
@@ -74,7 +78,7 @@ if __name__ == "__main__":
     print("Done!")
 
     print_console(f"Looking for the previous tag...")
-    previous_tag, version_id = find_previous_container_tag(response.json(), pr_tag)
+    previous_tag, version_id = find_previous_container_tag(response.json(), unique_tag)
     print("Done!")
 
     print_console(f"Deleting the previous container with tag ({previous_tag}) and version_id {version_id}...")
